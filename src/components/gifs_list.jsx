798736093a06a41513b2cb212efbad69571402e9
Lucien George <luciengeorge95@gmail.com>
2018-12-10 22:35:58 +0000

    redid the challenge

--- src/components/gifs_list.jsx
+++ src/components/gifs_list.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Gif from './gif.jsx';
+
 class GifsList extends Component {
   render() {
     return(
       <div className="gif-list">
-        {this.props.gifs.map(gif => <Gif id={gif.id} key={gif.id} selectGif={this.props.selectGif} />)}
+        {this.props.gifs.map(gif => <Gif id={gif.id} key={gif.id} selectGifFunction={this.props.selectGifFunction}/>)}
       </div>
-    )
+    );
   }
 }
 
